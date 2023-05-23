a7fafabe3f18b9dffc2f6a542181a052da67bef8
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-23 20:04:50 -0300

    Setup model classes

---
+++ app/src/main/java/com/example/todocompose/data/Priority.kt
@@ -0,0 +1,14 @@
+package com.example.todocompose.data
+
+import androidx.compose.ui.graphics.Color
+import com.example.todocompose.ui.theme.HighPriorityColor
+import com.example.todocompose.ui.theme.LowPriorityColor
+import com.example.todocompose.ui.theme.MediuPriorityColor
+import com.example.todocompose.ui.theme.NonePriorityColor
+
+enum class Priority(val color: Color) {
+    HIGH(HighPriorityColor),
+    MEDIUM(MediuPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(NonePriorityColor)
+}
