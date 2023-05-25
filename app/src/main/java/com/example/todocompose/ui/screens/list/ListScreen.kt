7d5ddf07b3ccb3692e8055dc74f0fda612828019
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-24 21:16:18 -0300

    Created topappbar and fixed dependencies conflicts

--- app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
@@ -1,26 +1,26 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.todocompose.ui.screens.list
 
 import android.annotation.SuppressLint
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todocompose.R
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ListScreen(
     navigateToTaskScreen: (Int) -> Unit
 ) {
     Scaffold(
+        topBar = {
+            ListAppBar()
+        },
         content = {},
         floatingActionButton = {
             ListFab(onFabClicked = navigateToTaskScreen)
