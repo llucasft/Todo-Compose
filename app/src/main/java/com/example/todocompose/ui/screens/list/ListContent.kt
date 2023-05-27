8693398437d5c893ea6a7afbcc14fe9d54daa737
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-26 21:46:56 -0300

    Task Item implementation

---
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListContent.kt
@@ -0,0 +1,106 @@
+package com.example.todocompose.ui.screens.list
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todocompose.data.models.Priority
+import com.example.todocompose.data.models.ToDoTask
+import com.example.todocompose.ui.theme.LARGE_PADDING
+import com.example.todocompose.ui.theme.PRIORITY_INDICATOR_SIZE
+import com.example.todocompose.ui.theme.TASK_ITEM_ELEVATION
+import com.example.todocompose.ui.theme.taskItemBackGroundColor
+import com.example.todocompose.ui.theme.taskItemTextColor
+
+@Composable
+fun ListContent() {
+
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TaskItem(
+    toDoTask: ToDoTask,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.taskItemBackGroundColor,
+        shape = RectangleShape,
+        elevation = TASK_ITEM_ELEVATION,
+        onClick = {
+            navigateToTaskScreen(toDoTask.id)
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(LARGE_PADDING)
+                .fillMaxWidth()
+        ) {
+            Row {
+                Text(
+                    modifier = Modifier.weight(8f),
+                    text = toDoTask.title,
+                    color = MaterialTheme.colors.taskItemTextColor,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1
+                )
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Canvas(
+                        modifier = Modifier
+                            .width(PRIORITY_INDICATOR_SIZE)
+                            .height(PRIORITY_INDICATOR_SIZE)
+                    ) {
+                        drawCircle(
+                            color = toDoTask.priority.color
+                        )
+                    }
+                }
+            }
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = toDoTask.description,
+                color = MaterialTheme.colors.taskItemTextColor,
+                style = MaterialTheme.typography.subtitle1,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun TaskItemPreview() {
+    TaskItem(
+        toDoTask = ToDoTask(
+            id = 0,
+            title = "GYM",
+            description = "Go to the gym",
+            priority = Priority.MEDIUM
+        ),
+        navigateToTaskScreen = {}
+    )
+}
