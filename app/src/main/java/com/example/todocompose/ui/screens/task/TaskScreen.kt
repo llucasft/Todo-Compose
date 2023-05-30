889442bb813fa75d86fb17d4f0f3692405f1d712
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-30 16:59:17 -0300

    Task content screen layout

--- app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
@@ -3,6 +3,7 @@ package com.example.todocompose.ui.screens.task
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.todocompose.data.models.Priority
 import com.example.todocompose.data.models.ToDoTask
 import com.example.todocompose.util.Action
 
@@ -19,6 +20,15 @@ fun TaskScreen(
                 navigateToListScreen = navigateToListScreen
             )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = "",
+                onTitleChange = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.LOW,
+                onPrioritySelected = {}
+            )
+        }
     )
 }
