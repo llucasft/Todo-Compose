61fe5884c6814fc4aa65f6e5b7d0726104998759
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-30 17:13:48 -0300

    Displaying task content dinamically

--- app/src/main/java/com/example/todocompose/ui/screens/list/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/destinations/TaskComposable.kt
@@ -1,6 +1,7 @@
 package com.example.todocompose.ui.screens.list.destinations
 
 import android.util.Log
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
@@ -27,8 +28,13 @@ fun NavGraphBuilder.taskComposable(
         val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
         sharedViewModel.getSelectedTask(taskId = taskId)
         val selectedTask by sharedViewModel.selectedTask.collectAsState()
+        
+        LaunchedEffect(key1 = selectedTask ) {
+            sharedViewModel.updateTaskFields(selectedTask = selectedTask)
+        }
 
         TaskScreen(
+            sharedViewModel = sharedViewModel,
             selectedTask = selectedTask,
             navigateToListScreen = navigateToListScreen
         )
--- app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
@@ -3,16 +3,23 @@ package com.example.todocompose.ui.screens.task
 import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import com.example.todocompose.data.models.Priority
 import com.example.todocompose.data.models.ToDoTask
+import com.example.todocompose.ui.viewmodels.SharedViewModel
 import com.example.todocompose.util.Action
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskScreen(
+    sharedViewModel: SharedViewModel,
     selectedTask: ToDoTask?,
     navigateToListScreen: (Action) -> Unit
 ) {
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
     Scaffold(
         topBar = {
             TaskAppBar(
@@ -22,12 +29,18 @@ fun TaskScreen(
         },
         content = {
             TaskContent(
-                title = "",
-                onTitleChange = {},
-                description = "",
-                onDescriptionChange = {},
-                priority = Priority.LOW,
-                onPrioritySelected = {}
+                title = title,
+                onTitleChange = {
+                    sharedViewModel.title.value = it
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
             )
         }
     )
