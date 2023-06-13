f3fa4eb48bbec64c1e184a1e002369e56e356dd4
Lucas Ferreira <lucasfteo@gmail.com>
2023-06-13 18:17:36 -0300

    Delete task

--- app/src/main/java/com/example/todocompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/todocompose/ui/viewmodels/SharedViewModel.kt
@@ -84,6 +84,18 @@ class SharedViewModel @Inject constructor(
         }
     }
 
+    private fun deleteTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.deleteTask(toDoTask = toDoTask)
+        }
+    }
+
     fun handleDatabaseActions(action: Action) {
         when (action) {
             Action.ADD -> {
@@ -95,7 +107,7 @@ class SharedViewModel @Inject constructor(
             }
 
             Action.DELETE -> {
-
+                deleteTask()
             }
 
             Action.DELETE_ALL -> {
